007aaec89203a0e73fa45aaef5bcfeecaca0a794
Jasmin Kozlica <Jasminkozlica20@gmail.com>
2024-09-13 14:43:02 +0200

    add person class

--- src/main/java/com/academy/my_first_spring_project/GreetingController.java
+++ src/main/java/com/academy/my_first_spring_project/GreetingController.java
@@ -3,11 +3,25 @@ package com.academy.my_first_spring_project;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class GreetingController {
     @GetMapping("/greeting")
-    public String greeting(Model model) {
+    public String greeting( @RequestParam(name ="n",required = false,defaultValue = "Welt") String someName,Model model) {
+
+        model.addAttribute("inputName",someName);
         return "greeting";
+    }
+    @GetMapping("/person")
+    @ResponseBody
+    public Person personJson(){
+        Person person = new Person();
+        person.setId(1L);
+        person.setName("Max");
+
+
+        return person;
     }
 }
